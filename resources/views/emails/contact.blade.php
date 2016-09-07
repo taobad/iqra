20de2080f43e5fbd33b02ac14a4c601a98ec26dc
Badmus Taofeeq <badmustaofeeq@gmail.com>
2016-09-07 21:14:49 +0100

    fixed untracked files

--- resources/views/emails/contact.blade.php
+++
@@ -1,6 +0,0 @@
-<h3>You have a new contact via the contact form</h3>
-<div>
-  {{$bodyMessage}}
-</div>
-
-<p> Sent via {{$email }} </p>
--- resources/views/layouts/partials/_footer.blade.php
+++
@@ -1,6 +0,0 @@
-<hr>
-<div class="row">
-    <div class="col-md-4 col-md-offset-4">
-        <p>Copyright © 2012 - 2016 Iqra College, Ilorin, Nigeria.</p>
-    </div>
-</div>
