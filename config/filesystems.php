1dd1b0da857cecf4edfdb1857ad4ff0be6d26a07
Badmus Taofeeq <badmustaofeeq@gmail.com>
2016-09-08 22:06:54 +0100

    blah blah blah

--- config/filesystems.php
+++ config/filesystems.php
@@ -50,7 +50,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => public_path(),
+            'root' => storage_path('app/public'),
             'visibility' => 'public',
         ],
 
