87b7540d398f7cb3bd2c3e0c757dd8f16333d26f
taobad <badmustaofeeq@gmail.com>
2020-03-20 07:13:11 +0000

    About layout changed

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -6,19 +6,18 @@
           <div class="col-md-5">
 
               <div class="panel panel-default">
+                  <div class="panel-heading">Vision</div>
+
+                  <div class="panel-body">
+                      <p><i>To be a leading world class institution</i></p>
+                  </div>
+              </div>
+              <div class="panel panel-default">
                   <div class="panel-heading">Mission</div>
 
                   <div class="panel-body">
                       <p><i>To imbue children in our care with faith, knowledge, skill, vision,
                           creativity and determination to continually impact positively their world</i></p>
-                  </div>
-              </div>
-
-              <div class="panel panel-default">
-                  <div class="panel-heading">Vision</div>
-
-                  <div class="panel-body">
-                      <p><i>To be a leading world class institution</i></p>
                   </div>
               </div>
           </div>
