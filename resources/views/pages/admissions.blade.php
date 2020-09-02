04b0e90c9d66d04f3859dd7b7280a62073733fd7
taobad <badmustaofeeq@gmail.com>
2020-09-02 08:23:08 +0100

    Admission

--- resources/views/pages/admissions.blade.php
+++ resources/views/pages/admissions.blade.php
@@ -4,28 +4,34 @@
 
 @section('content')
     <div class="row">
-        <div class="col-md-12">
-            <h1>Latest Admissions News
-                <span class="pull-right">
-                    <a href="{{route('application.prospect')}}" class="btn btn-primary btn-lg btn-block">Start/Retrieve an Application</a>
-                </span>
-            </h1>
+        <div class="col-md-4" style="margin-top: 15px; background: lightgray">
+            <div class="post">
+                <h1>Apply for Enrollmenent </h1>
+                <p> To apply for Enrollment, please pay the enrollment fee to a bank. Use the generated code to start and retrieve your
+                application. to start click on the button below.</p>
+                <a href="{{route('application.prospect')}}" class="btn btn-primary btn-lg btn-block">Start/Retrieve an Application</a>
+            </div>
         </div>
-    </div>
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            @foreach($posts as $post)
-                <div class="post">
-                    <h2>{{$post->title}}</h2>
-                    <h5>Published: {{date('M,j,Y h:ia',strtotime($post->created_at))}}</h5>
-                    <p>{{substr(strip_tags($post->body),0,300)}} {{strlen(strip_tags($post->body)) > 300 ? "...":""}}</p>
-                    <a href="{{route('news.single',$post->id)}}" class="btn btn-primary"> Read More...</a>
+        <div class="col-md-8">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Latest Admissions News</h1>
                 </div>
-                <hr>
-            @endforeach
-                <div class="text-center">
-                    {!! $posts->links() !!}
-                </div>
+            </div>
+            <div class="row">
+                @foreach($posts as $post)
+                    <div class="post">
+                        <h2>{{$post->title}}</h2>
+                        <h5>Published: {{date('M,j,Y h:ia',strtotime($post->created_at))}}</h5>
+                        <p>{{substr(strip_tags($post->body),0,300)}} {{strlen(strip_tags($post->body)) > 300 ? "...":""}}</p>
+                        <a href="{{route('news.single',$post->id)}}" class="btn btn-primary"> Read More...</a>
+                    </div>
+                    <hr>
+                @endforeach
+            </div>
+            <div class="text-center">
+                {!! $posts->links() !!}
+            </div>
         </div>
     </div>
 
