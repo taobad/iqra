474f94f90de81e20a79f203266f47d6facce19c5
taobad <badmustaofeeq@gmail.com>
2020-12-30 20:25:35 +0000

    Do not purify post body

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -72,7 +72,7 @@ class PostController extends Controller
 
         $post = new Post;
         $post->title = $request->title;
-        $post->body = Purifier::clean($request->body);
+        $post->body = $request->body;
         $post->eventdate = $request->eventdate;
 
         $post->save();
@@ -165,7 +165,7 @@ class PostController extends Controller
 
 
         $post->title = $request->title;
-        $post->body = Purifier::clean($request->body);
+        $post->body = $request->body;
         $post->eventdate = $request->eventdate;
 
         $post->save();
